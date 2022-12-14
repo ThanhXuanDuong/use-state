cd5b30d32273ee2c8f929551fc86a953b2faff22
thanhduong <xuanthanhduong94@gmail.com>
2022-12-14 08:35:22 +0100

    init: 1.Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] =useState<number>(0);
+  const [name, setName] =useState<string>("");
+
+  const increaseCount = ()=> setCount(count+1);
+  const decreaseCount = ()=> setCount(count-1);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{count}</h1>
+        <button onClick={increaseCount}>Click to increase</button>
+        <br/>
+        <button onClick={decreaseCount}>Click to decrease</button>
+        <br/>
+        <h1>{name}</h1>
+        <input onChange={e => setName(e.target.value)}/>
+
       </header>
     </div>
   );
